1340647913aa5d5e087a9489400a509aeaa6b205
= <pranavkumar0155@gmail.com>
2024-07-31 16:42:31 +0530

    added use client

--- app/page.js
+++ app/page.js
@@ -1,4 +1,6 @@
-import { useRouter } from 'next/router';
+"use client"
+
+import { useRouter } from 'next/navigation
 import { LuChevronsUpDown } from "react-icons/lu";
 
 export default function Home() {
